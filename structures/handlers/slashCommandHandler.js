7c85c8bb39b540677e706a4a1972c7cb895dc0cf
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:31:02 +0700

    Update slashCommandHandler.js

--- structures/handlers/slashCommandHandler.js
+++ structures/handlers/slashCommandHandler.js
@@ -57,13 +57,3 @@ module.exports = async (client) => {
     console.log(e.message);
   }
 }
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
