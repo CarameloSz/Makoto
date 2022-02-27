dd13bebb6f38d30dc1b6d3ea255c3ea4497a47ba
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:30:31 +0700

    Update mongoDBHandler.js

--- structures/handlers/mongoDBHandler.js
+++ structures/handlers/mongoDBHandler.js
@@ -37,13 +37,3 @@ module.exports = async (client) => {
     console.log(e)
   }
 };
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
