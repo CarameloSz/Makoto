43b3221a7de18967af68c3f741209cf703b03ade
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:26:54 +0700

    Update clientVariables.js

--- structures/handlers/clientVariables.js
+++ structures/handlers/clientVariables.js
@@ -17,13 +17,3 @@ module.exports = async (client) => {
 	
     client.logger(`Loaded Client_Variables`.brightGreen);
 }
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
