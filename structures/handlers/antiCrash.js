e0542c2adee3324d2e24db893aebced5a220ec6b
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:26:13 +0700

    Update antiCrash.js

--- structures/handlers/antiCrash.js
+++ structures/handlers/antiCrash.js
@@ -45,13 +45,3 @@ module.exports = async (client) => {
     
     client.logger(`Loaded AntiCrash`.brightGreen);
 };
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
