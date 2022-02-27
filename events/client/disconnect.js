67cea321ca388a1ffb94c14b56465ae34cbc8a24
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:33:08 +0700

    Update disconnect.js

--- events/client/disconnect.js
+++ events/client/disconnect.js
@@ -5,13 +5,3 @@ module.exports = {
         client.logger(`You have been disconnected at ${new Date()}.`.dim);
     }
 }
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
