88f01f2e9a6403d7a257944eca93f3c351f6f71d
Utero <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:24:58 +0700

    Update connected.js

--- structures/handlers/mongoDB_Events/connected.js
+++ structures/handlers/mongoDB_Events/connected.js
@@ -5,13 +5,3 @@ module.exports = {
     client.logger(`Connected to MongoDB Database!`.bold.brightGreen)
   }
 }
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
