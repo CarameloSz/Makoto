f44aa79ba8205414de383c53c79c98afa2fb96f5
Kiara <91861739+Kashimaa129@users.noreply.github.com>
2022-02-27 16:36:34 +0700

    Update reconnecting.js

--- events/client/reconnecting.js
+++ events/client/reconnecting.js
@@ -5,13 +5,3 @@ module.exports = {
     client.logger(`Reconnceting at ${new Date()}.`.bgYellow.black);
   }
 }
-
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
